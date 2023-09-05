05b9e392ccefc0a02c849d2232a869b5db4a7b7c
Diego Felipe Quijano Zuñiga <dfqz93@hotmail.com>
2023-09-05 16:26:23 -0500

    rutas

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,15 +1,36 @@
 const {Router} = require('express');
 const { usuariosGet, usuariosPost, usuariosPut, usuariosPatch, usuariosDelete } = require('../controllers/usuarios');
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { esRolValido, emailExiste, existeUsuarioPorid } = require('../helpers/db-validators');
+const role = require('../models/role');
 
 const router = Router();
 
-router.get('/', usuariosGet)
+router.get('/', usuariosGet) // Primero se envia la ruta, segundo los middlewares y tercero los controladores
 
-router.put('/:id', usuariosPut)
+router.put('/:id', [
+    check('id', 'No es un id valido').isMongoId(),
+    check('id').custom(existeUsuarioPorid),
+    check('rol').custom(esRolValido),
+    validarCampos
+],  usuariosPut)
 
-router.post('/', usuariosPost)
+router.post('/', [
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('correo', 'El correo no es valido').isEmail(),
+    check('correo').custom(emailExiste),
+    check('password', 'El password debe de ser mas de 6 letras').isLength({min: 6}),
+    // check('rol', 'no es un rol permitido').isIn('ADMIN_ROLE', 'USER_ROLE'),
+    check('rol').custom(esRolValido),
+    validarCampos
+], usuariosPost)
 
-router.delete('/', usuariosDelete)
+router.delete('/:id', [
+    check('id', 'No es un id valido').isMongoId(),
+    check('id').custom(existeUsuarioPorid),
+    validarCampos
+], usuariosDelete)
 
 router.patch('/', usuariosPatch)
 
