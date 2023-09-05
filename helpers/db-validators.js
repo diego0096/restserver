ec906843c559464573f901c02141da2272aafa81
Diego Felipe Quijano Zuñiga <dfqz93@hotmail.com>
2023-09-05 16:25:11 -0500

    validaciones al cargar datos a la base de datos

---
+++ helpers/db-validators.js
@@ -0,0 +1,30 @@
+const {model} = require('mongoose')
+const Role = require('../models/role');
+const Usuario = require('../models/usuario')
+
+const esRolValido = async(rol = '') => {
+    const existeRol = await Role.findOne({rol});
+    if(!existeRol) {
+        throw new Error(`El rol ${rol} no esta registrado en la BD`);
+    }
+}
+
+const emailExiste = async(correo = '') => {
+    const existeEmail = await Usuario.findOne({correo});
+    if(existeEmail) {
+        throw new Error(`El correo: ${correo}, ya esta registrado`)
+    }
+}
+
+const existeUsuarioPorid = async(id) => {
+    const existeUsuario = await Usuario.findById(id);
+    if(!existeUsuario) {
+        throw new Error(`El id: ${id}, no existe`)
+    }
+}
+
+module.exports = {
+    esRolValido,
+    emailExiste,
+    existeUsuarioPorid,
+}
