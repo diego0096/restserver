840dff125100a47cd31fd6c9ec7d9b8299d3e39f
Diego Felipe Quijano Zuñiga <dfqz93@hotmail.com>
2023-09-07 12:09:16 -0500

    controllers

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -63,11 +63,7 @@ const usuariosPost = async (req, res = response) => {
 const usuariosDelete = async(req, res = response) => {
     const {id} = req.params;
 
-    // Borrar fisicamente
-    // const usuario = await Usuario.findByIdAndDelete(id);
-
-    //
-    const usuario = await Usuario.findByIdAndUpdate(id, {estado: false})
+    const usuario = await Usuario.findByIdAndUpdate(id, {estado: false});
 
     res.json({
         usuario
