aa4564662884a58f0300098d476e4865e6bab606
Diego Felipe Quijano Zuñiga <dfqz93@hotmail.com>
2023-10-12 11:39:02 -0500

    add categoria routes endpoints

---
+++ routes/categorias.js
@@ -0,0 +1,48 @@
+const { Router } = require('express');
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT, esAdminRole } = require('../middlewares');
+const { crearCategoria, obtenerCategorias, obtenerCategoria, actualizarCategoria, borrarCategoria } = require('../controllers/categorias');
+const { existeCategoriaPorid } = require('../helpers/db-validators');
+
+/**
+ * {{url}}/api/categorias
+ */
+
+const router = Router();
+
+// Obtener todas las categorias - publico
+router.get('/', obtenerCategorias)
+
+// Obtener una categoria por id - publico
+router.get('/:id', [
+    check('id', 'No es un id de Mongo valido').isMongoId(),
+    check('id').custom(existeCategoriaPorid),
+    validarCampos
+], obtenerCategoria)
+
+// Crear categoria - ptivado - cualquier persona con un token valido
+router.post('/', [
+    validarJWT,
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    validarCampos
+], crearCategoria);
+
+// Actualizar cualquiera con token valido 
+router.put('/:id', [
+    validarJWT,
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('id').custom(existeCategoriaPorid),
+    validarCampos
+], actualizarCategoria)
+
+// Borrar una categoria - Admin 
+router.delete('/:id', [
+    validarCampos,
+    esAdminRole,
+    check('id', 'No es un id de Mongo valido').isMongoId(),
+    check('id').custom(existeCategoriaPorid),
+    validarCampos
+], borrarCategoria)
+
+module.exports = router;
