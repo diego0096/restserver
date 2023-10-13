f90bdc173183aace8cbcf0b458a306aac270b39a
Diego Felipe Quijano Zuñiga <dfqz93@hotmail.com>
2023-10-13 12:03:47 -0500

    fix google verification

--- helpers/google-verify.js
+++ helpers/google-verify.js
@@ -1,8 +1,8 @@
 const { OAuth2Client } = require('google-auth-library');
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
-async function googleVerify(token = '') {
+async function googleVerify(idToken = '') {
     const ticket = await client.verifyIdToken({
-        idToken: token,
+        idToken,
         audience: process.env.GOOGLE_CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
         // Or, if multiple clients access the backend:
         //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
@@ -10,13 +10,13 @@ async function googleVerify(token = '') {
     const payload = ticket.getPayload();
     console.log(payload)
 
-    const {name, picture, email} = ticket.getPayload();
-
-    return {
+    const {
         name: name,
         img: picture,
         correo: email
-    }
+    } = ticket.getPayload();
+
+    return { name, picture, email }
 }
 module.exports = {
     googleVerify
