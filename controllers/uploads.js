1698c2cc8a26acb459bbf92d5cc0504f160bd8d0
Diego Felipe Quijano Zuñiga <dfqz93@hotmail.com>
2023-10-25 21:12:47 -0500

    upload images

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,7 +1,9 @@
-const path = require('path')
-const { response } = require("express");
+const path = require('path');
+const {v4: uuidv4} = require('uuid');
 
-const cargarArchivo = (req, res = response) => {
+const { response, request } = require("express");
+
+const cargarArchivo = (req = request, res = response) => {
 
     if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
         res.status(400).json({msg: 'No hay archivos que subir'});
@@ -9,8 +11,20 @@ const cargarArchivo = (req, res = response) => {
     }
 
     const {archivo} = req.files;
+    const nombreCortado = archivo.name.split('.');
 
-    const uploadPath = path.join(__dirname, '../uploads/', archivo.name);
+    const extension = nombreCortado[nombreCortado.length - 1];
+
+    // vsalidar la extensión
+    const extensionesValidas = ['png', 'jpg', 'jpeg', 'gif'];
+    if(!extensionesValidas.includes(extension)) {
+        return res.status(400).json({
+            msg: `La extensión ${extension} no es permitida, ${extensionesValidas}`
+        })
+    }
+
+    const nombreTemp = uuidv4() + '.' + extension;
+    const uploadPath = path.join(__dirname, '../uploads/', nombreTemp);
 
     archivo.mv(uploadPath, (err) => {
         if (err) {
