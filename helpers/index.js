133530150daca1505f3370dd73d0c3ad2fcdcdce
Diego Felipe Quijano Zuñiga <dfqz93@hotmail.com>
2023-10-27 14:35:55 -0500

    export helpers

---
+++ helpers/index.js
@@ -0,0 +1,11 @@
+const debValidators = require('./db-validators');
+const generarJWT = require('./generar-jwt');
+const googleVerify = require('./google-verify');
+const subirArchivo = require('./subir-archivo');
+
+module.exports = {
+    ...debValidators,
+    ...generarJWT,
+    ...googleVerify,
+    ...subirArchivo,
+}
