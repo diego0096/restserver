47d3cf00355e38b7b3d81cb54653397ea649636d
Diego Felipe Quijano Zuñiga <dfqz93@hotmail.com>
2023-10-31 20:22:50 -0500

    add put upload service

--- helpers/db-validators.js
+++ helpers/db-validators.js
@@ -38,10 +38,22 @@ const existeProductoPorid = async(id) => {
     }
 }
 
+/**
+ *  Validar colecciones permitidas
+ */
+const coleccionesPermitidas = (coleccion = '', colecciones = []) => {
+    const incluida = colecciones.includes(coleccion);
+    if(!incluida) {
+        throw new Error(`La colección ${coleccion} no es permitida, ${colecciones}`);
+    }
+    return true;
+}
+
 module.exports = {
     esRolValido,
     emailExiste,
     existeUsuarioPorid,
     existeCategoriaPorid,
-    existeProductoPorid
+    existeProductoPorid,
+    coleccionesPermitidas
 }
