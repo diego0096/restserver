018e4a4a1963be78be45ae2b99acd4cb14d9d96d
Diego Felipe Quijano Zuñiga <dfqz93@hotmail.com>
2023-10-31 20:23:05 -0500

    add put upload service

--- routes/uploads.js
+++ routes/uploads.js
@@ -1,10 +1,17 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
-const { cargarArchivo } = require('../controllers/uploads');
+const { cargarArchivo, actualizarimagen } = require('../controllers/uploads');
+const { coleccionesPermitidas } = require('../helpers');
 
 const router = Router();
 
 router.post('/', cargarArchivo);
+
+router.put('/:coleccion/:id', [
+    check('id', 'El id de ser de mongo').isMongoId(),
+    check('coleccion').custom(c => coleccionesPermitidas(c, ['usuarios', 'productos'])),
+    validarCampos
+], actualizarimagen)
 
 module.exports = router;
