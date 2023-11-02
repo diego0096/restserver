f34ee878544f167f22370ec5ad1983ceabe9e37e
Diego Felipe Quijano Zuñiga <dfqz93@hotmail.com>
2023-11-01 20:17:41 -0500

    update files service

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,29 +1,70 @@
 const { response, request } = require("express");
 const { subirArchivo } = require("../helpers");
+const { Usuario, Producto } = require("../models");
+const path = require('path');
+const fs = require('fs');
 
-const cargarArchivo = async(req = request, res = response) => {
-
-    if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
-        res.status(400).json({msg: 'No hay archivos que subir'});
-        return;
-    }
+const cargarArchivo = async (req = request, res = response) => {
 
     try {
         // const nombre = await subirArchivo(req.files, ['txt', 'md'], 'textos');
         const nombre = await subirArchivo(req.files, undefined, 'imgs');
-        res.json({nombre})
+        res.json({ nombre })
     } catch (msg) {
-        res.status(400).json({msg})
+        res.status(400).json({ msg })
     }
 }
 
-const actualizarimagen = async(req, res = response) => {
-    const {id, coleccion} = req.params;
-    
-    res.json({id, coleccion})
+const actualizarImagen = async (req, res = response) => {
+
+    const { id, coleccion } = req.params;
+
+    let modelo;
+
+    switch (coleccion) {
+        case 'usuarios':
+            modelo = await Usuario.findById(id);
+            if (!modelo) {
+                return res.status(400).json({
+                    msg: `No existe un usuario con el id ${id}`
+                });
+            }
+
+            break;
+
+        case 'productos':
+            modelo = await Producto.findById(id);
+            if (!modelo) {
+                return res.status(400).json({
+                    msg: `No existe un producto con el id ${id}`
+                });
+            }
+
+            break;
+
+        default:
+            return res.status(500).json({ msg: 'Se me olvidó validar esto' });
+    }
+
+    // Limpiar imagenes previas
+    if(modelo.img) {
+        // Hay que borrar la imagen del servidor
+        const pathImagen = path.join(__dirname, '../uploads', coleccion, modelo.img);
+        if(fs.existsSync(pathImagen)) {
+            fs.unlinkSync(pathImagen);
+        }
+    }
+
+    const nombre = await subirArchivo(req.files, undefined, coleccion);
+    modelo.img = nombre;
+
+    await modelo.save();
+
+
+    res.json(modelo);
 }
 
 module.exports = {
     cargarArchivo,
-    actualizarimagen,
+    actualizarImagen,
 }
