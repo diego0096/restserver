c1c17144e27f6be5f7f5ffecaab3dff66f402fcb
Diego Felipe Quijano Zuñiga <dfqz93@hotmail.com>
2023-11-01 20:17:56 -0500

    update files service

--- routes/uploads.js
+++ routes/uploads.js
@@ -1,17 +1,19 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
-const { cargarArchivo, actualizarimagen } = require('../controllers/uploads');
+const { cargarArchivo, actualizarImagen } = require('../controllers/uploads');
 const { coleccionesPermitidas } = require('../helpers');
+const { validarArchivoSubir } = require('../middlewares');
 
 const router = Router();
 
-router.post('/', cargarArchivo);
+router.post('/', validarArchivoSubir, cargarArchivo);
 
 router.put('/:coleccion/:id', [
+    validarArchivoSubir,
     check('id', 'El id de ser de mongo').isMongoId(),
     check('coleccion').custom(c => coleccionesPermitidas(c, ['usuarios', 'productos'])),
     validarCampos
-], actualizarimagen)
+], actualizarImagen)
 
 module.exports = router;
