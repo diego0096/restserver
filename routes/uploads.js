89f682f548d99c7a2d72dd80f95fa752982f795d
Diego Felipe Quijano Zuñiga <dfqz93@hotmail.com>
2023-11-02 21:08:10 -0500

    get file certein route

--- routes/uploads.js
+++ routes/uploads.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
-const { cargarArchivo, actualizarImagen } = require('../controllers/uploads');
+const { cargarArchivo, actualizarImagen, mostrarImagen } = require('../controllers/uploads');
 const { coleccionesPermitidas } = require('../helpers');
 const { validarArchivoSubir } = require('../middlewares');
 
@@ -15,5 +15,11 @@ router.put('/:coleccion/:id', [
     check('coleccion').custom(c => coleccionesPermitidas(c, ['usuarios', 'productos'])),
     validarCampos
 ], actualizarImagen)
+
+router.get('/:coleccion/:id', [
+    check('id', 'El id debe de ser de mongo').isMongoId(),
+    check('coleccion').custom(c => coleccionesPermitidas(c, ['usuarios', 'productos'])),
+    validarCampos
+], mostrarImagen)
 
 module.exports = router;
