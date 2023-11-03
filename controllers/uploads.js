098f44f3b21f3a88487c543891f1b7946b911d38
Diego Felipe Quijano Zuñiga <dfqz93@hotmail.com>
2023-11-02 21:07:20 -0500

    get file certein route

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -47,10 +47,10 @@ const actualizarImagen = async (req, res = response) => {
     }
 
     // Limpiar imagenes previas
-    if(modelo.img) {
+    if (modelo.img) {
         // Hay que borrar la imagen del servidor
         const pathImagen = path.join(__dirname, '../uploads', coleccion, modelo.img);
-        if(fs.existsSync(pathImagen)) {
+        if (fs.existsSync(pathImagen)) {
             fs.unlinkSync(pathImagen);
         }
     }
@@ -64,7 +64,49 @@ const actualizarImagen = async (req, res = response) => {
     res.json(modelo);
 }
 
+const mostrarImagen = async(req = request, res = response) => {
+    const { id, coleccion } = req.params;
+
+    let modelo;
+
+    switch (coleccion) {
+        case 'usuarios':
+            modelo = await Usuario.findById(id);
+            if (!modelo) {
+                return res.status(400).json({
+                    msg: `No existe un usuario con el id ${id}`
+                });
+            }
+
+            break;
+
+        case 'productos':
+            modelo = await Producto.findById(id);
+            if (!modelo) {
+                return res.status(400).json({
+                    msg: `No existe un producto con el id ${id}`
+                });
+            }
+
+            break;
+
+        default:
+            return res.status(500).json({ msg: 'Se me olvidó validar esto' });
+    }
+
+    // Limpiar imagenes previas
+    if (modelo.img) {
+        // Hay que borrar la imagen del servidor
+        const pathImagen = path.join(__dirname, '../uploads', coleccion, modelo.img);
+        if (fs.existsSync(pathImagen)) {
+            return res.sendFile(pathImagen)
+        }
+    }
+    res.json({msg: 'Falta el placeholder'})
+}
+
 module.exports = {
     cargarArchivo,
     actualizarImagen,
+    mostrarImagen
 }
